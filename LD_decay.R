2652717b887cc04c2d182b8732ba56cf7a2d0250
taylorcurryaz <taylorcurry@arizona.edu>
2025-07-29 12:46:04 -0700

    Update LD_decay.R
    added titles, added equation

--- LD_decay.R
+++ LD_decay.R
@@ -16,10 +16,16 @@ des_my_bins <- "des_1na.ld_decay_bins"
 # read in data
 des_ld_bins <- read_tsv(des_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(des_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth(method = "lm")+
-  theme_bw()+stat_poly_eq(use_label(c("eq")))
+ggplot(des_ld_bins, aes(distance, avg_R2)) +
+  geom_line() +
+  xlab("Distance (bp)") +
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000)+
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("DES")
 
 ###WAT
 # set path
@@ -27,10 +33,16 @@ wat_my_bins <- "wat_1na.ld_decay_bins"
 # read in data
 wat_ld_bins <- read_tsv(wat_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(wat_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()+theme_bw()+stat_poly_eq(use_label(c("eq")))
-
+ggplot(wat_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") +
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth()+
+  theme_bw()+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("WAT")
 
 ####COL
 #set path
@@ -38,9 +50,16 @@ col_my_bins <- "col_Scaffold_1.ld_decay_bins"
 # read in data
 col_ld_bins <- read_tsv(col_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(col_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth(method = "lm")+theme_bw()+stat_poly_eq(use_label(c("eq")))
+ggplot(col_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("COL")
 
 ###DIA
 #set path
@@ -48,9 +67,15 @@ dia_my_bins <- "dia_1na.ld_decay_bins"
 # read in data
 dia_ld_bins <- read_tsv(dia_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(dia_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth(method = "lm")+theme_bw()+stat_poly_eq(use_label(c("eq")))
+ggplot(dia_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("DIA")
 
 ###GIL
 #set path
@@ -58,9 +83,16 @@ gil_my_bins <- "gil_1na.ld_decay_bins"
 # read in data
 gil_ld_bins <- read_tsv(gil_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(gil_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(gil_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("GIL")
 
 ###GOL
 #set path
@@ -68,9 +100,16 @@ gol_my_bins <- "gol_1na.ld_decay_bins"
 # read in data
 gol_ld_bins <- read_tsv(gol_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(gol_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr,)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(gol_ld_bins, aes(distance, avg_R2)) +
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr,)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("GOL")
 
 ###LEB
 #set path
@@ -78,9 +117,15 @@ leb_my_bins <- "leb_1na.ld_decay_bins"
 # read in data
 leb_ld_bins <- read_tsv(leb_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(leb_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(leb_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("LEB")
 
 ###NEE
 #set path
@@ -88,9 +133,16 @@ Nee_my_bins <- "nee_1na.ld_decay_bins"
 # read in data
 Nee_ld_bins <- read_tsv(Nee_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(Nee_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(Nee_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") +
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000)+
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("NEE")
 
 ###ORC
 #set path
@@ -98,9 +150,15 @@ orc_my_bins <- "orc_1na.ld_decay_bins"
 # read in data
 orc_ld_bins <- read_tsv(orc_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(orc_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(orc_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("ORC")
 
 ###ORO
 #set path
@@ -108,9 +166,16 @@ oro_my_bins <- "oro_1na.ld_decay_bins"
 # read in data
 oro_ld_bins <- read_tsv(oro_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(oro_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(oro_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("ORO")
 
 ###RB
 #set path
@@ -118,18 +183,31 @@ rb_my_bins <- "rb_1na.ld_decay_bins"
 # read in data
 rb_ld_bins <- read_tsv(rb_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(rb_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(rb_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("RB")
 
 ###RES
 res_my_bins <- "res_1naa.ld_decay_bins"
 # read in data
 res_ld_bins <- read_tsv(res_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(res_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(res_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("RES")
 
 ###SIE
 #set path
@@ -137,9 +215,16 @@ sie_my_bins <- "sie_1na.ld_decay_bins"
 # read in data
 sie_ld_bins <- read_tsv(sie_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(sie_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(sie_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") +
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("SIE")
 
 ###TRI
 #set path
@@ -147,18 +232,31 @@ tri_my_bins <- "tri_1na.ld_decay_bins"
 # read in data
 tri_ld_bins <- read_tsv(tri_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(tri_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(tri_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("TRI")
 
 ###UKI
 uki_my_bins <- "uki_1na.ld_decay_bins"
 # read in data
 uki_ld_bins <- read_tsv(uki_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(uki_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(uki_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("UKI")
 
 ###VET
 #set path
@@ -166,9 +264,16 @@ vet_my_bins <- "vet_1nad.ld_decay_bins"
 # read in data
 vet_ld_bins <- read_tsv(vet_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(vet_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth()+theme_bw()
+ggplot(vet_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("VET")
 
 ###YRE
 #set path
@@ -176,12 +281,18 @@ yre_my_bins <- "yre_1nad.ld_decay_bins"
 # read in data
 yre_ld_bins <- read_tsv(yre_my_bins, show_col_types = FALSE)
 # plot LD decay
-ggplot(yre_ld_bins, aes(distance, avg_R2)) + geom_line() +
-  xlab("Distance (bp)") + ylab(expression(italic(r)^2)) +
-  facet_wrap(~chr)+xlim(0,50000) + geom_smooth(method = "lm")+theme_bw()
+ggplot(yre_ld_bins, aes(distance, avg_R2)) + 
+  geom_line() +
+  xlab("Distance (bp)") + 
+  ylab(expression(italic(r)^2)) +
+  facet_wrap(~chr)+
+  xlim(0,50000) + 
+  geom_smooth(method = "lm")+
+  theme_bw()+
+  stat_poly_eq(use_label(c("eq")))+
+  ggtitle("YRE")
 
-
-######################
+#calculate mean and std dev
 mean_ld_col <- col_ld_bins %>%
  group_by(chr) %>% 
   summarize(mean = mean(avg_R2), sd = sd(avg_R2))
@@ -191,6 +302,62 @@ mean_ld_des <- des_ld_bins %>%
   summarize(mean = mean(avg_R2), sd = sd(avg_R2))
 
 mean_ld_wat <- wat_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_dia <- dia_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_gil <- gil_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_gol <- gol_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_leb <- leb_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_nee <- Nee_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_orc <- orc_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_oro <- oro_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_rb <- rb_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_res <- res_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_sie <- sie_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_tri <- tri_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_uki <- uki_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_vet <- vet_ld_bins %>%
+  group_by(chr) %>% 
+  summarize(mean = mean(avg_R2), sd = sd(avg_R2))
+
+mean_ld_yre <- yre_ld_bins %>%
   group_by(chr) %>% 
   summarize(mean = mean(avg_R2), sd = sd(avg_R2))
 
